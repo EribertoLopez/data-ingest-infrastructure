50867957f88e4674cd2135e96dfe0f4dd9a48e27
Guillaume Manderscheid <guillaumemanderscheid@Guillaumes-MacBook-Pro.local>
2021-02-22 15:46:57 -0800

    Adding processing steps to personal branch

--- lambdas/dataservice.py
+++ lambdas/dataservice.py
@@ -105,3 +105,144 @@ class S3File(io.RawIOBase):
 
     def readable(self):
         return True
+
+def processHTS(event, context):
+    dynamodb = boto3.client('dynamodb')
+    metadata = findMetadata()
+    df, fileFormat = readFile(event['filename'])
+    if fileFormat == 'Long':
+        df = processLongFormat(df)
+        print(addMetadata(df, metadata).columns)
+    if fileFormat == 'Matrix':
+        df = processMatrixFormat(df)
+        print(addMetadata(df, metadata).columns)
+
+    for row in df.values[:10] :
+        well, value, channel, id_, platebarcode, runid, warpid = row[0], row[1], row[2], row[3], row[4], row[5], row[6]
+        add_to_db = dynamodb.put_item(
+                TableName='htsprocessed', 
+                Item={
+                    'well' : {'S' : str(well)},
+                    'value' : {'N' : str(value)},
+                    'channel' : {'S' : str(channel)},
+                    'id' : {'N' : str(id_)},
+                    'platebarcode' : {'S' : str(platebarcode)},
+                    'runId' : {'S' : str(runid)},
+                    'warpId' : {'S' : str(warpid)}
+                    })
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            'hello_from': 'something.',
+            'dataType': 'HTS_DATA',
+            'route': '/HTS-Data',
+            'event': event
+        })
+    }
+
+def readFile(filename):
+    """
+    
+    """
+    SkiprowsLongFormat = 16 
+    filepath = 's3://{}/{}'.format('strateos-ds-hts-dummy', filename)
+    fs = s3fs.S3FileSystem()
+
+    try : 
+        return pd.read_csv(filepath, header=None), 'Matrix'
+    except :
+        return pd.read_csv(filepath, header=None, skiprows=SkiprowsLongFormat, encoding="ISO-8859-1"), 'Long'
+    
+def parseFirstColumn(series):
+    """
+    
+    """
+    parser = lambda x : [x.split(':')[0], int(x.split(':')[1])]
+    return series.apply(parser).apply(pd.Series)
+
+def flatteningChannels(df):
+    """
+    
+    """
+    flattenedChannels = pd.DataFrame()
+    for channel in ['ChannelA', 'ChannelB', 'ChannelRatio']:
+        temp = df[['Well', channel]].rename(columns={channel:'Value'})
+        temp['Channel'] = channel.split('l')[-1]
+        flattenedChannels = pd.concat([flattenedChannels, temp])
+    return flattenedChannels
+
+def processLongFormat(df):
+    """
+    
+    """
+    df.rename(columns={0:'ToParse', 1:'ChannelB', 2:'ChannelRatio'}, inplace=True)
+    parsed = parseFirstColumn(df['ToParse']).rename(columns={0:'Well', 1:'ChannelA'})
+    df.drop('ToParse', 
+            axis=1, 
+            inplace=True)
+    df = pd.concat([parsed, df], axis=1)
+    df = flatteningChannels(df)
+
+    return df
+
+def getWellLetterConfig(df):
+    """
+    
+    """
+    numberOfLetters = int(df.shape[0] / 3)
+    formatedAlphabet = ascii_letters.split('z')[1] + ascii_letters.split('z')[0]
+    return formatedAlphabet[:numberOfLetters]
+
+def getChannelList(indexSeries):
+    """
+    
+    """
+    numberOfChannels = 3
+    switchChannelIndex = indexSeries.shape[0] / numberOfChannels
+    channels = []
+    for index in indexSeries: 
+        if index < switchChannelIndex :
+            channels.append('A')
+        elif index < switchChannelIndex * 2 :
+            channels.append('B')
+        else : 
+            channels.append('Ratio')
+    return channels
+
+def processMatrixFormat(df):
+    """
+    
+    """
+    wellLetterConfig = getWellLetterConfig(df)
+    df['Channel'] = getChannelList(df.index)
+    newStruc = []
+    for channel in df.Channel.unique():
+        for row, letter in zip(df[df['Channel'] == channel].drop('Channel', axis=1).values, wellLetterConfig) : 
+            for index, well_value in enumerate(row) : 
+                if index + 1 < 10:
+                    newStruc.append([letter + "0" + str(index + 1), well_value, channel])
+                else : 
+                    newStruc.append([letter + str(index + 1), well_value, channel ])
+
+    return pd.DataFrame(newStruc, columns=['Well', 'Value', 'Channel'])
+
+def findMetadata():
+    """
+    
+    """
+    metadata = {'id': 1,
+            'plateBarcode': 'AM00000656',
+            'runId': 'r1f57rf77jbdsd', 
+            'warpId': 'warpid'}
+    return metadata
+
+def addMetadata(df, metadata):
+    """
+    
+    """
+    df['id'] = metadata['id']
+    df['plateBarcode'] = metadata['plateBarcode']
+    df['runId'] = metadata['runId']
+    df['warpId'] = metadata['warpId']
+    return df 
