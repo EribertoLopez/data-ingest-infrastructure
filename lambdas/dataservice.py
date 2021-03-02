5c970e27e074260efd1e5798171a7622a71dc99c
Guillaume Manderscheid <guillaumemanderscheid@Guillaumes-MacBook-Pro.local>
2021-03-02 11:07:54 -0800

    Adding read in s3 and dump in dynamodb functions

--- lambdas/dataservice.py
+++ lambdas/dataservice.py
@@ -1,42 +1,44 @@
 import requests
 import json
 import boto3
-
+import os
 import codecs
 import csv
 import io
 import zipfile
+import s3fs
+import pandas as pd 
 
-
-INGEST_S3 = 'dataIngestor-infrastructure-dev-HTSTable-1OH8J5K3CA3D9'
 def process(event, context):
-    print(f'Get DynamoDB Client...')
-    ddb = boto3.resource('dynamodb')
-    table = ddb.Table(INGEST_S3)
+    # Get DynamoDb table size
+    client = boto3.client('dynamodb', 'us-west-2')
+    table_name = client.list_tables()['TableNames'][1]
+    table = boto3.resource('dynamodb').Table(table_name)
+    table_length = len(table.scan()['Items'])
+    print(table_length)
     
-    print('S3 client')
-    print('find object')
+    # Get s3 data 
     bucket_name = 'strateos-ingest-bucket-dev'
     filename = 'test.zip'
     s3, object = get_s3_object(bucket_name, filename)
-    print(f'Process .CSV data')
     s3_file = S3File(object)
+    
+    # Read the s3 file and dump in dynamo
     with zipfile.ZipFile(s3_file) as zf:
         for datapath in zf.namelist(): # lists all files in zip
             with zf.open(datapath, 'r') as infile:
                 reader = csv.reader(io.TextIOWrapper(infile, 'utf-8'))
 
                 headers = next(reader)
+                partition_key_index = headers.index("measurement")
+                print(partition_key_index, 'part_key index')
                 for row in reader:
+                    row[partition_key_index] = str(int(row[partition_key_index]) + table_length)
                     item = {header: val for header, val in zip(headers, row)}
                     # TODO: Is there a better way to batch instead using multiple PUT calls
                     ddbResponse = table.put_item(
                         Item=item
                     )
-
-
-
-    print(f'PUT with client')
 
     return {
         "statusCode": 200,
@@ -106,143 +108,3 @@ class S3File(io.RawIOBase):
     def readable(self):
         return True
 
-def processHTS(event, context):
-    dynamodb = boto3.client('dynamodb')
-    metadata = findMetadata()
-    df, fileFormat = readFile(event['filename'])
-    if fileFormat == 'Long':
-        df = processLongFormat(df)
-        print(addMetadata(df, metadata).columns)
-    if fileFormat == 'Matrix':
-        df = processMatrixFormat(df)
-        print(addMetadata(df, metadata).columns)
-
-    for row in df.values[:10] :
-        well, value, channel, id_, platebarcode, runid, warpid = row[0], row[1], row[2], row[3], row[4], row[5], row[6]
-        add_to_db = dynamodb.put_item(
-                TableName='htsprocessed', 
-                Item={
-                    'well' : {'S' : str(well)},
-                    'value' : {'N' : str(value)},
-                    'channel' : {'S' : str(channel)},
-                    'id' : {'N' : str(id_)},
-                    'platebarcode' : {'S' : str(platebarcode)},
-                    'runId' : {'S' : str(runid)},
-                    'warpId' : {'S' : str(warpid)}
-                    })
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            'hello_from': 'something.',
-            'dataType': 'HTS_DATA',
-            'route': '/HTS-Data',
-            'event': event
-        })
-    }
-
-def readFile(filename):
-    """
-    
-    """
-    SkiprowsLongFormat = 16 
-    filepath = 's3://{}/{}'.format('strateos-ds-hts-dummy', filename)
-    fs = s3fs.S3FileSystem()
-
-    try : 
-        return pd.read_csv(filepath, header=None), 'Matrix'
-    except :
-        return pd.read_csv(filepath, header=None, skiprows=SkiprowsLongFormat, encoding="ISO-8859-1"), 'Long'
-    
-def parseFirstColumn(series):
-    """
-    
-    """
-    parser = lambda x : [x.split(':')[0], int(x.split(':')[1])]
-    return series.apply(parser).apply(pd.Series)
-
-def flatteningChannels(df):
-    """
-    
-    """
-    flattenedChannels = pd.DataFrame()
-    for channel in ['ChannelA', 'ChannelB', 'ChannelRatio']:
-        temp = df[['Well', channel]].rename(columns={channel:'Value'})
-        temp['Channel'] = channel.split('l')[-1]
-        flattenedChannels = pd.concat([flattenedChannels, temp])
-    return flattenedChannels
-
-def processLongFormat(df):
-    """
-    
-    """
-    df.rename(columns={0:'ToParse', 1:'ChannelB', 2:'ChannelRatio'}, inplace=True)
-    parsed = parseFirstColumn(df['ToParse']).rename(columns={0:'Well', 1:'ChannelA'})
-    df.drop('ToParse', 
-            axis=1, 
-            inplace=True)
-    df = pd.concat([parsed, df], axis=1)
-    df = flatteningChannels(df)
-
-    return df
-
-def getWellLetterConfig(df):
-    """
-    
-    """
-    numberOfLetters = int(df.shape[0] / 3)
-    formatedAlphabet = ascii_letters.split('z')[1] + ascii_letters.split('z')[0]
-    return formatedAlphabet[:numberOfLetters]
-
-def getChannelList(indexSeries):
-    """
-    
-    """
-    numberOfChannels = 3
-    switchChannelIndex = indexSeries.shape[0] / numberOfChannels
-    channels = []
-    for index in indexSeries: 
-        if index < switchChannelIndex :
-            channels.append('A')
-        elif index < switchChannelIndex * 2 :
-            channels.append('B')
-        else : 
-            channels.append('Ratio')
-    return channels
-
-def processMatrixFormat(df):
-    """
-    
-    """
-    wellLetterConfig = getWellLetterConfig(df)
-    df['Channel'] = getChannelList(df.index)
-    newStruc = []
-    for channel in df.Channel.unique():
-        for row, letter in zip(df[df['Channel'] == channel].drop('Channel', axis=1).values, wellLetterConfig) : 
-            for index, well_value in enumerate(row) : 
-                if index + 1 < 10:
-                    newStruc.append([letter + "0" + str(index + 1), well_value, channel])
-                else : 
-                    newStruc.append([letter + str(index + 1), well_value, channel ])
-
-    return pd.DataFrame(newStruc, columns=['Well', 'Value', 'Channel'])
-
-def findMetadata():
-    """
-    
-    """
-    metadata = {'id': 1,
-            'plateBarcode': 'AM00000656',
-            'runId': 'r1f57rf77jbdsd', 
-            'warpId': 'warpid'}
-    return metadata
-
-def addMetadata(df, metadata):
-    """
-    
-    """
-    df['id'] = metadata['id']
-    df['plateBarcode'] = metadata['plateBarcode']
-    df['runId'] = metadata['runId']
-    df['warpId'] = metadata['warpId']
-    return df 
